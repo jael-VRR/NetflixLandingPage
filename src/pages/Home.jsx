1d49b53bd979e515c893027689a9164236438239
jael-VRR <jrodriguezruidias@gmail.com>
2021-02-03 16:47:10 -0500

    add event of change view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "../styles/Home.css";
 import Navbar from "../components/Navbar.jsx";
+import Content from "../components/Content.jsx";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  let history = useHistory();
+
+  const handleChange = () => {
+    history.push("/mediaPlayerV");
+  };
   return (
     <div>
       <header>
         <Navbar />
       </header>
-      <main></main>
+      <main className="box-video">
+        <Content handleChange={handleChange} />
+      </main>
     </div>
   );
 }
